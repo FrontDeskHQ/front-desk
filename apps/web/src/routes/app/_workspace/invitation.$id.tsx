30b53914319d2b710fd0953d8e3dc12ed493f846
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-10-03 19:53:18 -0300

    chore: move invitation file route

--- apps/web/src/routes/app/_workspace/invitation.$id.tsx
+++ apps/web/src/routes/app/_workspace/invitation.$id.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/app/invitation/$id")({
+export const Route = createFileRoute("/app/_workspace/invitation/$id")({
   component: RouteComponent,
 });
 
