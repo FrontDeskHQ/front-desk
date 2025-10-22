616e64abf538e39d17c38a47b1c07c557c54e3fb
Daniel Lima <danielmlimag@gmail.com>
2025-10-22 01:01:05 -0300

    remove bg

--- apps/web/src/routes/support/{-$slug}/threads/index.tsx
+++ apps/web/src/routes/support/{-$slug}/threads/index.tsx
@@ -29,10 +29,10 @@ function RouteComponent() {
   const organization = Route.useLoaderData().organization;
 
   return (
-    <div className="w-full bg-neutral-950">
+    <div className="w-full">
       <Navbar />
       <div className=" mx-auto w-full px-4 py-8 sm:px-6 lg:px-8">
-        <div className="mb-8 flex items-center gap-6 rounded-lg bg-neutral-900 p-6 shadow-sm border border-neutral-800">
+        <div className="mb-8 flex items-center gap-6 rounded-lg p-6 shadow-sm border border-neutral-800">
           <Avatar
             variant="org"
             size="xxl"
@@ -47,14 +47,14 @@ function RouteComponent() {
         <div className="mb-6">
           <Input
             placeholder="Search threads..."
-            className="w-full bg-neutral-900 border-neutral-800 text-neutral-50 placeholder:text-neutral-400"
+            className="w-full border-neutral-800 text-neutral-50 placeholder:text-neutral-400"
           />
         </div>
         <div className="space-y-2">
           {organization?.threads.map((thread) => (
             <div
               key={thread.id}
-              className="rounded-lg bg-neutral-900 border border-neutral-800 shadow-sm transition-all hover:border-neutral-700 hover:shadow-md"
+              className="rounded-lg border border-neutral-800 shadow-sm transition-all hover:border-neutral-700 hover:shadow-md"
             >
               <ListItem threadId={thread.id} />
             </div>
