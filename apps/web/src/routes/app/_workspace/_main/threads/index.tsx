6312c14ca639a5efe61de50455f5dabdb99c4e79
Daniel Lima <danielmlimag@gmail.com>
2025-10-24 14:26:13 -0300

    separate threads card logic

--- apps/web/src/routes/app/_workspace/_main/threads/index.tsx
+++ apps/web/src/routes/app/_workspace/_main/threads/index.tsx
@@ -1,7 +1,57 @@
+import type { InferLiveObject } from "@live-state/sync";
 import { useLiveQuery } from "@live-state/sync/client";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { Avatar } from "@workspace/ui/components/avatar";
+import {
+  Filter,
+  type FilterOptions,
+  type FilterValue,
+} from "@workspace/ui/components/blocks/filter";
+import { Button } from "@workspace/ui/components/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@workspace/ui/components/card";
+import {
+  PriorityIndicator,
+  priorityText,
+  StatusIndicator,
+  statusValues,
+} from "@workspace/ui/components/indicator";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@workspace/ui/components/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@workspace/ui/components/select";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@workspace/ui/components/tooltip";
+import { getFirstTextContent, safeParseJSON } from "@workspace/ui/lib/tiptap";
+import { formatRelativeTime } from "@workspace/ui/lib/utils";
+import type { schema } from "api/schema";
 import { useAtomValue } from "jotai/react";
-import { ThreadsCard } from "~/components/threads/threads-card";
+import {
+  ArrowDownWideNarrow,
+  ArrowUpNarrowWide,
+  CircleUser,
+  Settings2,
+} from "lucide-react";
+import { useState } from "react";
+import { CreateThread } from "~/components/devtools/create-thread";
 import { activeOrganizationAtom } from "~/lib/atoms";
 import { query } from "~/lib/live-state";
 
@@ -16,9 +66,218 @@ function RouteComponent() {
     query.organization.where({ id: currentOrg?.id }).include({ threads: true }),
   )?.[0];
 
+  const organizationUsers = useLiveQuery(
+    query.organizationUser
+      .where({ organizationId: organization?.id })
+      .include({ user: true }),
+  );
+
+  const [filter, setFilter] = useState<FilterValue>({});
+
+  const orderByOptions = [
+    { label: "Created", value: "createdAt" },
+    { label: "Last message", value: "updatedAt" }, // TODO fix when live-state supports deep sorting
+    { label: "Priority", value: "priority" },
+    { label: "Status", value: "status" },
+  ];
+
+  let threadsQuery = query.thread.where({
+    organizationId: organization?.id,
+  });
+
+  if (filter && Object.keys(filter).some((key) => filter[key]?.length > 0)) {
+    threadsQuery = threadsQuery.where(
+      Object.fromEntries(
+        Object.entries(filter).map(([key, values]) => [key, { $in: values }]),
+      ),
+    );
+  }
+
+  const filterOptions: FilterOptions = {
+    status: {
+      label: "Status",
+      key: "status",
+      icon: <StatusIndicator status={0} />,
+      options: Object.entries(statusValues).map(([statusKey, value]) => {
+        const status = Number(statusKey);
+        return {
+          label: value.label,
+          value: status,
+          icon: <StatusIndicator status={status} />,
+        };
+      }),
+    },
+    priority: {
+      label: "Priority",
+      key: "priority",
+      icon: <PriorityIndicator priority={2} />,
+      options: Object.entries(priorityText).map(([priorityKey, value]) => {
+        const priority = Number(priorityKey);
+        return {
+          label: value,
+          value: priority,
+          icon: <PriorityIndicator priority={priority} />,
+        };
+      }),
+    },
+    assignedUserId: {
+      label: "Assigned User",
+      key: "assignedUserId",
+      icon: <CircleUser className="size-4" />,
+      options: (organizationUsers ?? []).map((user) => ({
+        label: user.user.name,
+        value: user.userId,
+        icon: <Avatar variant="user" size="md" fallback={user.user.name} />,
+      })),
+    },
+  };
+
+  const [orderBy, setOrderBy] = useState<string>("createdAt");
+  const [orderDirection, setOrderDirection] = useState<"asc" | "desc">("asc");
+
+  // TODO Deep include thread messages and assigned user when live-state supports it
+  // TODO reverse sort messages by createdAt
+  const threads = useLiveQuery(
+    threadsQuery
+      .include({ messages: true, assignedUser: true })
+      .orderBy(
+        orderBy as keyof InferLiveObject<typeof schema.thread>,
+        orderDirection,
+      ),
+  );
+
   if (!organization) {
     return null;
   }
 
-  return <ThreadsCard organization={organization} />;
+  return (
+    <Card className="bg-muted/30">
+      <CardHeader>
+        <CardTitle className="gap-4">Threads</CardTitle>
+        <CardAction side="right">
+          {/* TODO: Implement search functionality when live-state supports full text search */}
+          {/* <Search placeholder="Search" /> */}
+
+          <Filter
+            options={filterOptions}
+            value={filter}
+            onValueChange={setFilter}
+          />
+
+          <Popover>
+            <PopoverTrigger>
+              <Button variant="ghost" size="sm">
+                <Settings2 />
+                Display
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent
+              className="p-4 flex flex-col gap-4"
+              positionerProps={{ align: "end" }}
+            >
+              <div className="flex w-full items-center gap-2">
+                <div className="mr-auto">Order by</div>
+                <Select
+                  value={orderBy}
+                  onValueChange={(value) => setOrderBy(value as string)}
+                  items={orderByOptions}
+                >
+                  <SelectTrigger className="w-48" data-size="sm">
+                    <SelectValue />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {orderByOptions.map((option) => (
+                      <SelectItem key={option.value} value={option.value}>
+                        {option.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() =>
+                          setOrderDirection(
+                            orderDirection === "asc" ? "desc" : "asc",
+                          )
+                        }
+                        className="size-8"
+                      >
+                        {orderDirection === "asc" ? (
+                          <ArrowDownWideNarrow />
+                        ) : (
+                          <ArrowUpNarrowWide />
+                        )}
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>Change order direction</TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </CardAction>
+      </CardHeader>
+      <CardContent className="overflow-y-auto gap-0 items-center">
+        {threads?.map((thread) => (
+          <Link
+            key={thread.id}
+            to={"/app/threads/$id"}
+            params={{ id: thread.id }}
+            className="w-full max-w-5xl flex flex-col p-3 gap-2 hover:bg-muted"
+          >
+            <div className="flex justify-between">
+              <div className="flex items-center gap-2">
+                <Avatar variant="user" size="md" fallback={"P"} />
+                <div>{thread?.name}</div>
+              </div>
+              <div className="flex items-center gap-2">
+                {thread?.assignedUserId ? (
+                  <Avatar
+                    variant="user"
+                    size="md"
+                    fallback={thread.assignedUser?.name}
+                  />
+                ) : (
+                  <CircleUser className="size-4" />
+                )}
+                <PriorityIndicator priority={thread?.priority ?? 0} />
+                <StatusIndicator status={thread?.status ?? 0} />
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <span className="text-muted-foreground">
+                <span className="font-medium">
+                  {/* TODO update when live-state supports deep includes */}
+                  {/* {thread?.messages?.[thread?.messages?.length - 1]?.author?.name} */}
+                  Author:&nbsp;
+                </span>
+                <span className="truncate">
+                  {getFirstTextContent(
+                    safeParseJSON(
+                      thread?.messages?.[thread?.messages?.length - 1]
+                        ?.content ?? "",
+                    ),
+                  )}
+                </span>
+              </span>
+              <div className="text-muted-foreground">
+                {thread?.createdAt
+                  ? formatRelativeTime(thread?.createdAt as Date)
+                  : null}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </CardContent>
+      {import.meta.env.MODE === "development" && (
+        <CardFooter>
+          <CreateThread />
+        </CardFooter>
+      )}
+    </Card>
+  );
 }
--- apps/web/src/routes/support/{-$slug}/threads/index.tsx
+++ apps/web/src/routes/support/{-$slug}/threads/index.tsx
@@ -1,6 +1,15 @@
-import { createFileRoute, notFound } from "@tanstack/react-router";
+import type { InferLiveObject } from "@live-state/sync";
+import { useLiveQuery } from "@live-state/sync/client";
+import { createFileRoute, Link, notFound } from "@tanstack/react-router";
 import { Avatar } from "@workspace/ui/components/avatar";
 import { Button } from "@workspace/ui/components/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@workspace/ui/components/card";
 import { Header } from "@workspace/ui/components/header";
 import {
   Pagination,
@@ -11,8 +20,34 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@workspace/ui/components/pagination";
-import { ThreadsCard } from "~/components/threads/threads-card";
-import { fetchClient } from "~/lib/live-state";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@workspace/ui/components/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@workspace/ui/components/select";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@workspace/ui/components/tooltip";
+import { getFirstTextContent, safeParseJSON } from "@workspace/ui/lib/tiptap";
+import { formatRelativeTime } from "@workspace/ui/lib/utils";
+import type { schema } from "api/schema";
+import {
+  ArrowDownWideNarrow,
+  ArrowUpNarrowWide,
+  Settings2,
+} from "lucide-react";
+import { useState } from "react";
+import { fetchClient, query } from "~/lib/live-state";
 
 export const Route = createFileRoute("/support/{-$slug}/threads/")({
   component: RouteComponent,
@@ -42,6 +77,29 @@ function RouteComponent() {
   // TODO: Update URL to reflect real organization discord link
   const integrationPaths = { discord: "https://discord.com/invite/acme" };
 
+  const orderByOptions = [
+    { label: "Created", value: "createdAt" },
+    { label: "Last message", value: "updatedAt" }, // TODO fix when live-state supports deep sorting
+  ];
+
+  const threadsQuery = query.thread.where({
+    organizationId: organization?.id,
+  });
+
+  const [orderBy, setOrderBy] = useState<string>("createdAt");
+  const [orderDirection, setOrderDirection] = useState<"asc" | "desc">("asc");
+
+  // TODO Deep include thread messages and assigned user when live-state supports it
+  // TODO reverse sort messages by createdAt
+  const threads = useLiveQuery(
+    threadsQuery
+      .include({ messages: true })
+      .orderBy(
+        orderBy as keyof InferLiveObject<typeof schema.thread>,
+        orderDirection,
+      ),
+  );
+
   if (!organization) {
     return null;
   }
@@ -70,7 +128,109 @@ function RouteComponent() {
             </Button>
           </div>
         </div>
-        <ThreadsCard organization={organization} hidePrivateProps={true} />
+        <Card className="bg-muted/30">
+          <CardHeader>
+            <CardTitle className="gap-4">Threads</CardTitle>
+            <CardAction side="right">
+              {/* TODO: Implement search functionality when live-state supports full text search */}
+              {/* <Search placeholder="Search" /> */}
+              <Popover>
+                <PopoverTrigger>
+                  <Button variant="ghost" size="sm">
+                    <Settings2 />
+                    Display
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent
+                  className="p-4 flex flex-col gap-4"
+                  positionerProps={{ align: "end" }}
+                >
+                  <div className="flex w-full items-center gap-2">
+                    <div className="mr-auto">Order by</div>
+                    <Select
+                      value={orderBy}
+                      onValueChange={(value) => setOrderBy(value as string)}
+                      items={orderByOptions}
+                    >
+                      <SelectTrigger className="w-48" data-size="sm">
+                        <SelectValue />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {orderByOptions.map((option) => (
+                          <SelectItem key={option.value} value={option.value}>
+                            {option.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Button
+                            variant="outline"
+                            size="sm"
+                            onClick={() =>
+                              setOrderDirection(
+                                orderDirection === "asc" ? "desc" : "asc",
+                              )
+                            }
+                            className="size-8"
+                          >
+                            {orderDirection === "asc" ? (
+                              <ArrowDownWideNarrow />
+                            ) : (
+                              <ArrowUpNarrowWide />
+                            )}
+                          </Button>
+                        </TooltipTrigger>
+                        <TooltipContent>Change order direction</TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            </CardAction>
+          </CardHeader>
+          <CardContent className="overflow-y-auto gap-0 items-center">
+            {threads?.map((thread) => (
+              <Link
+                key={thread.id}
+                to={"/app/threads/$id"}
+                params={{ id: thread.id }}
+                className="w-full max-w-5xl flex flex-col p-3 gap-2 hover:bg-muted"
+              >
+                <div className="flex justify-between">
+                  <div className="flex items-center gap-2">
+                    <Avatar variant="user" size="md" fallback={"P"} />
+                    <div>{thread?.name}</div>
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <span className="text-muted-foreground">
+                    <span className="font-medium">
+                      {/* TODO update when live-state supports deep includes */}
+                      {/* {thread?.messages?.[thread?.messages?.length - 1]?.author?.name} */}
+                      Author:&nbsp;
+                    </span>
+                    <span className="truncate">
+                      {getFirstTextContent(
+                        safeParseJSON(
+                          thread?.messages?.[thread?.messages?.length - 1]
+                            ?.content ?? "",
+                        ),
+                      )}
+                    </span>
+                  </span>
+                  <div className="text-muted-foreground">
+                    {thread?.createdAt
+                      ? formatRelativeTime(thread?.createdAt as Date)
+                      : null}
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </CardContent>
+        </Card>
         <Pagination>
           <PaginationContent>
             <PaginationItem>
