a000a4fce896f3bf9c417ab218a0d5a0b5e1e315
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-10-29 00:00:06 -0300

    chore: prepare for deployment

--- apps/web/src/lib/live-state.ts
+++ apps/web/src/lib/live-state.ts
@@ -7,7 +7,7 @@ import { schema } from "api/schema";
 import { authClient } from "./auth-client";
 
 const { client, store } = createClient<Router>({
-  url: "ws://localhost:3333/api/ls/ws",
+  url: process.env.LIVE_STATE_WS_URL ?? "ws://localhost:3333/api/ls/ws",
   schema,
   credentials: async () => ({
     token: (await authClient.oneTimeToken.generate()).data?.token ?? "",
@@ -25,10 +25,8 @@ const { query, mutate } = store;
 export { client, mutate, query };
 
 export const fetchClient = createFetchClient<Router>({
-  url:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/api/ls"
-      : "http://localhost:3333/api/ls",
+  // This needs to be in the same domain as the web app, since we need to share cookies
+  url: process.env.LIVE_STATE_API_URL ?? "http://localhost:3000/api/ls",
   schema,
   credentials: createIsomorphicFn()
     .server(() => Object.fromEntries(getRequestHeaders()))
