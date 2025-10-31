22e8e8902d9ac9a6448a5db95dbdd7336dbe61aa
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-10-31 18:38:27 -0300

    chore(web): add wrangler setup

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -20,6 +21,7 @@ export default defineConfig({
     tsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
     viteReact(),
   ],
