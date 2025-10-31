2812d23f55d48e8c361d064fd121daddc438f79e
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-10-31 01:40:39 -0300

    fix: svg props

--- packages/ui/src/components/logo.tsx
+++ packages/ui/src/components/logo.tsx
@@ -42,15 +42,15 @@ function LogoSeparator({ className }: { className?: string }) {
     <svg
       data-testid="geist-icon"
       height="16"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
       viewBox="0 0 16 16"
       width="16"
       className={cn("size-5", className)}
     >
       <title>Logo Icon</title>
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M4.01526 15.3939L4.3107 14.7046L10.3107 0.704556L10.6061 0.0151978L11.9849 0.606077L11.6894 1.29544L5.68942 15.2954L5.39398 15.9848L4.01526 15.3939Z"
         fill="rgba(255, 255, 255, 0.14)"
       ></path>
