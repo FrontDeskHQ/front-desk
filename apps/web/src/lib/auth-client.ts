dede198999639f99ac411b03a3035712a97f6e92
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-11-01 14:26:54 -0300

    chore: make auth server url public

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -2,6 +2,6 @@ import { oneTimeTokenClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.AUTH_SERVER_BASE_URL ?? "http://localhost:3333", // The base URL of your auth server
+  baseURL: process.env.VITE_AUTH_SERVER_BASE_URL ?? "http://localhost:3333", // The base URL of your auth server
   plugins: [oneTimeTokenClient()],
 });
