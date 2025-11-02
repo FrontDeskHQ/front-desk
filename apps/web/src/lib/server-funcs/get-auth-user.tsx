92c7cb0e738dcee7fd73351dc680912bafc614c5
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-11-01 21:14:24 -0300

    temp: debug auth

--- apps/web/src/lib/server-funcs/get-auth-user.tsx
+++ apps/web/src/lib/server-funcs/get-auth-user.tsx
@@ -5,14 +5,17 @@ import { authClient } from "../auth-client";
 export const getAuthUser = createServerFn({
   method: "GET",
 }).handler(async () => {
-  console.log("getRequestHeaders()", getRequestHeaders());
+  console.log(
+    "getRequestHeaders()",
+    JSON.stringify(getRequestHeaders(), null, 2),
+  );
   const res = await authClient.getSession({
     fetchOptions: {
       headers: getRequestHeaders() as HeadersInit,
     },
   });
 
-  console.log("res", res);
+  console.log("res", JSON.stringify(res, null, 2));
 
   return res.data;
 });
