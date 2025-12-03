3b28dacb2886c99ec6c004e329ecd764743c8e42
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-12-02 21:41:27 -0300

    chore: update indicator styles

--- packages/ui/src/components/indicator.tsx
+++ packages/ui/src/components/indicator.tsx
@@ -5,20 +5,20 @@ export const PriorityIndicator = ({ priority }: { priority: number }) => {
   return (
     <div
       className={cn(
-        "h-4 w-[17px] p-px flex justify-between items-center",
+        "h-[11px] w-[14px] flex justify-between items-center",
         priority > 0 && "items-end",
       )}
     >
       <div
         className={cn(
-          "w-[3px] h-1/3 rounded-full bg-primary/50",
+          "w-[3px] h-3/5 rounded-full bg-primary/50",
           priority >= 1 && "bg-primary",
           priority === 0 && "h-1",
         )}
       />
       <div
         className={cn(
-          "w-[3px] h-2/3 rounded-full bg-primary/50",
+          "w-[3px] h-4/5 rounded-full bg-primary/50",
           priority >= 2 && "bg-primary",
           priority === 0 && "h-1",
         )}
