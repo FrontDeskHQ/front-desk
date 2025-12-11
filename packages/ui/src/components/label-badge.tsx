2e8da1b952471eb4a8ec31662028de81fafbe96b
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-12-11 19:01:25 -0300

    use new colors

--- packages/ui/src/components/label-badge.tsx
+++ packages/ui/src/components/label-badge.tsx
@@ -21,7 +21,9 @@ const LabelBadge = React.forwardRef<HTMLDivElement, LabelBadgeProps>(
           className="size-2 rounded-full shrink-0"
           style={{ backgroundColor: color }}
         />
-        <div className="truncate grow shrink text-muted-foreground">{name}</div>
+        <div className="truncate grow shrink text-foreground-secondary">
+          {name}
+        </div>
       </div>
     );
   },
