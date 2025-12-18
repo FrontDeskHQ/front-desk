a59e7c41cc068c8da27867423bcbeab6368883b6
Daniel Lima <danielmlimag@gmail.com>
2025-12-18 13:03:40 -0300

    feat: rewrite GitHub app

--- apps/github/src/env.ts
+++ apps/github/src/env.ts
@@ -1,2 +1,10 @@
 import dotenv from "dotenv";
+
 dotenv.config({ path: [".env.local", ".env"] });
+
+// Define required environment variables
+const envVars = ["GITHUB_WEBHOOK_SECRET"];
+
+for (const varName of envVars) {
+  if (!process.env[varName]) console.log(`${varName} not set`);
+}
