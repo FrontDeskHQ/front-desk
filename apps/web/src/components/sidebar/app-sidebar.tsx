f88669f7373738b15ef3c555c18711bcf20e66df
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-12-27 21:11:02 -0300

    chore(web): remove feature flag for widget

--- apps/web/src/components/sidebar/app-sidebar.tsx
+++ apps/web/src/components/sidebar/app-sidebar.tsx
@@ -1,9 +1,7 @@
-import { useFlag } from "@reflag/react-sdk";
 import { Link, useMatches } from "@tanstack/react-router";
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarHeader,
@@ -12,7 +10,7 @@ import {
   SidebarMenuItem,
   SidebarRail,
 } from "@workspace/ui/components/sidebar";
-import { Book, MessageCircleQuestion, MessagesSquare } from "lucide-react";
+import { MessagesSquare } from "lucide-react";
 import { OrgSwitcher } from "./organization-switcher";
 
 const items: { title: string; url: string; icon: React.ComponentType<any> }[] =
@@ -26,8 +24,6 @@ const items: { title: string; url: string; icon: React.ComponentType<any> }[] =
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const matches = useMatches();
-
-  const { isEnabled: isWidgetEnabled, config } = useFlag("widget");
 
   return (
     <Sidebar variant="inset" className="bg-none">
@@ -60,26 +56,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         {/* <NavFavorites favorites={data.favorites} /> */}
         {/* <NavWorkspaces workspaces={data.workspaces} /> */}
         {/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
-        {!isWidgetEnabled && (
-          <SidebarFooter>
-            <SidebarMenuButton asChild>
-              <a href="/docs" target="_blank" rel="noopener noreferrer">
-                <Book />
-                Docs
-              </a>
-            </SidebarMenuButton>
-            <SidebarMenuButton asChild>
-              <a
-                href="https://discord.gg/5MDHqKHrHr"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <MessageCircleQuestion />
-                Support
-              </a>
-            </SidebarMenuButton>
-          </SidebarFooter>
-        )}
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
