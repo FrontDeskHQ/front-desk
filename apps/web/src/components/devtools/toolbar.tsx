bc85fd0729a5e01f2d51800fc71620ef31a15671
Pedro Costa <scosta.pedro.manoel@gmail.com>
2026-01-09 11:32:53 -0300

    add live-state log

--- apps/web/src/components/devtools/toolbar.tsx
+++ apps/web/src/components/devtools/toolbar.tsx
@@ -1,5 +1,11 @@
-export function Toolbar() {
+"use client";
+
+import { LiveStateLog } from "./live-state-log";
+
+export const Toolbar = () => {
   return (
-    <div className="w-screen h-6 bg-background-secondary border-t shrink-0 flex"></div>
+    <div className="w-screen h-6 bg-background-secondary border-t shrink-0 flex font-mono gap-2 items-center px-2 z-10">
+      <LiveStateLog />
+    </div>
   );
-}
+};
