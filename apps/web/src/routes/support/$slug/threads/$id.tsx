b3c36afe85d4255f2dcd01189f7381d4be3df0f5
claudinxch <claudiohenrique0909@gmail.com>
2026-02-02 14:09:01 -0300

    fix(ui): show 'no labels' indicator on public portal

--- apps/web/src/routes/support/$slug/threads/$id.tsx
+++ apps/web/src/routes/support/$slug/threads/$id.tsx
@@ -415,16 +415,17 @@ function RouteComponent() {
             <div className="flex flex-col gap-2">
               <div className="text-muted-foreground text-xs">Labels</div>
               <div className="flex flex-col gap-1.5">
-                <div className="flex items-center gap-2 flex-wrap">
-                  {thread?.labels
-                    ?.filter((tl) => tl.enabled && !!tl.label?.enabled)
-                    .map((threadLabel) => (
-                      <LabelBadge
-                        key={threadLabel.label.id}
-                        name={threadLabel.label.name}
-                        color={threadLabel.label.color}
-                      />
-                    ))}
+                <div className="flex items-center px-1.5 gap-2 flex-wrap">
+                  {thread?.labels?.length > 0 ?
+                    thread.labels.filter((tl) => tl.enabled && !!tl.label?.enabled)
+                      .map((threadLabel) => (
+                        <LabelBadge
+                          key={threadLabel.label.id}
+                          name={threadLabel.label.name}
+                          color={threadLabel.label.color}
+                        />
+                      )) : <span className="text-muted-foreground">No labels</span>
+                  }
                 </div>
               </div>
             </div>
